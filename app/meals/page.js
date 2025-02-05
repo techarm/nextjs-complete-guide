cd2f71dbc0ca5a4288a49865d845114819d222aa
techarm <techarmdev@gmail.com>
2025-02-05 11:59:09 +0900

    created dev db and show data list

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
-import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-export default function MealsPage() {
+import classes from './page.module.css';
+
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -15,7 +18,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
