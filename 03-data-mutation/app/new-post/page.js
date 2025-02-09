a1b9b33ede3c5228a7e2b379aac84e186dc63637
techarm <techarmdev@gmail.com>
2025-02-09 13:28:35 +0900

    useFormStatus HookでSubmitの状態を管理

--- 03-data-mutation/app/new-post/page.js
+++ 03-data-mutation/app/new-post/page.js
@@ -1,18 +1,21 @@
+import FormSubmit from '@/components/form-submit';
 import { storePost } from '@/lib/posts';
 
 export default function NewPostPage() {
   async function createPost(formData) {
-    "use server";
+    'use server';
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
-    storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log(title, content);
+    // storePost({
+    //   imageUrl: '',
+    //   title,
+    //   content,
+    //   userId: 1,
+    // });
   }
 
   return (
@@ -25,20 +28,14 @@ export default function NewPostPage() {
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
+          <input type="file" accept="image/png, image/jpeg" id="image" name="image" />
         </p>
         <p className="form-control">
           <label htmlFor="content">Content</label>
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
