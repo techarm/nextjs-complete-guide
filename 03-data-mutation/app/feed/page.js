f0c559fd8418064350975b0c3ba9bce23af64189
techarm <techarmdev@gmail.com>
2025-02-13 22:33:10 +0900

    Adding Dynamic Page Metadata

--- 03-data-mutation/app/feed/page.js
+++ 03-data-mutation/app/feed/page.js
@@ -1,6 +1,14 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata() {
+  const posts = await getPosts();
+  return {
+    title: `Browse all out ${posts.length} posts`,
+    description: 'Browse all our posts.',
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
