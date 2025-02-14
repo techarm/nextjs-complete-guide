c25a28c27ccb0f2a88cd4a93adffa4a143509160
techarm <techarmdev@gmail.com>
2025-02-14 22:07:41 +0900

    Adding Validating User Input

--- 04-authentication/components/auth-form.js
+++ 04-authentication/components/auth-form.js
@@ -1,8 +1,15 @@
+'use client';
+
+import { useActionState } from 'react';
 import Link from 'next/link';
 
+import { signup } from '@/actions/auth-actions';
+
 export default function AuthForm() {
+  const [formState, formAction] = useActionState(signup, {});
+
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -14,10 +21,15 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {formState.errors && (
+        <ul id="form-errors">
+          {Object.keys(formState.errors).map((error) => (
+            <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>
+      )}
       <p>
-        <button type="submit">
-          Create Account
-        </button>
+        <button type="submit">Create Account</button>
       </p>
       <p>
         <Link href="/">Login with existing account.</Link>
