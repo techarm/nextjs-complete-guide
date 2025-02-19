ed6beadc75c5742eb53f305cf5a59c3166a71047
techarm <techarmdev@gmail.com>
2025-02-19 20:32:51 +0900

    Adding Event Search Page

--- 05-prj-events/app/events/page.js
+++ 05-prj-events/app/events/page.js
@@ -1,5 +1,26 @@
-function AllEvent() {
-  return <h1>test</h1>;
+'use client';
+
+import { Fragment } from 'react';
+import { redirect } from 'next/navigation';
+
+import EventList from '@/components/events/event-list';
+import EventsSearch from '@/components/events/events-search';
+import { getAllEvents } from '@/dummy-data';
+
+function AllEvents() {
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    redirect(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
 }
 
-export default AllEvent;
+export default AllEvents;
