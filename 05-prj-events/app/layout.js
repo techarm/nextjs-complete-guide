6a046575e87d96aa2d27952cba220d77242cf1c4
techarm <techarmdev@gmail.com>
2025-02-19 20:11:53 +0900

    Adding Main Header

--- 05-prj-events/app/layout.js
+++ 05-prj-events/app/layout.js
@@ -1,3 +1,4 @@
+import MainHeader from '@/components/header/main-header';
 import './globals.css';
 
 export const metadata = {
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainHeader />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
